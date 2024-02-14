5ad68e83750dab8f3b72947ab7ec58535668b391
Ali Rezaei <im1.bitcode@gmail.com>
2024-02-14 15:30:49 +0330

    home page design

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@ import { links } from "../utils/constant";
 
 <template>
   <header>
-    <nav class="bg-slate-100 border-gray-200 px-4 md:px-6 py-4">
+    <nav class="bg-gray-100 px-4 md:px-6 py-4">
       <div
         class="flex flex-wrap justify-center md:justify-between items-center mx-auto max-w-screen-xl"
       >
@@ -18,6 +18,7 @@ import { links } from "../utils/constant";
           <ul class="flex mt-4 md:gap-x-8 md:mt-0">
             <li v-for="(link, index) in links" :key="index">
               <router-link
+                active-class="text-black"
                 :to="link.href"
                 class="flex items-center gap-1 py-2 pr-4 pl-3 text-gray-500 hover:text-gray-600 md:p-0 transition"
               >
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,5 +5,34 @@ const title = ref("Home Page");
 </script>
 
 <template>
-  {{ title }}
+  <div class="flex flex-col items-center">
+    <h1 class="text-xl font-semibold mb-5">{{ title }}</h1>
+
+    <div class="w-full max-w-2xl mb-3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita laborum
+      reprehenderit voluptatum sunt blanditiis dolor architecto ipsum? Beatae
+      voluptatibus sunt deleniti quos ex a atque iste perferendis facilis quia
+      totam earum voluptates molestias obcaecati at repudiandae explicabo quam,
+      porro illo! Suscipit earum a nemo voluptates quasi reprehenderit quo ipsum
+      iusto? Harum minus eos voluptate repudiandae molestiae, officiis vitae
+      ipsa ducimus, architecto magnam autem inventore eum ipsum minima cumque!
+      Quo quos sed dignissimos consectetur hic mollitia perspiciatis laboriosam
+      cupiditate harum, iusto assumenda possimus praesentium? Nesciunt possimus
+      blanditiis illum enim aliquam! Consectetur, illum in. Temporibus sed
+      officiis vero commodi nisi obcaecati minima.
+    </div>
+
+    <div class="flex gap-5">
+      <button
+        class="bg-gray-900 py-2 rounded-lg text-white hover:bg-gray-800 transition w-20"
+      >
+        Users
+      </button>
+      <button
+        class="bg-gray-100 py-2 rounded-lg hover:bg-gray-200 transition w-20"
+      >
+        Posts
+      </button>
+    </div>
+  </div>
 </template>
